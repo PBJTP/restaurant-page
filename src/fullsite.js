004b161216cbca51f95df662643c4028969a1101
PBJTP <jplypick1990@gmail.com>
2023-01-29 23:21:50 -0800

    Finish home page styling for full screen. Add font

---
+++ src/fullsite.js
@@ -0,0 +1,10 @@
+// generate header with nava elements. assign classes. append to div
+
+// generate background image. assign classes. append to div.
+
+// generate footer. assign classes. append to div.
+
+// run exported home page function to fill content
+
+// add event listeners to nav buttons with functionality to change content
+
